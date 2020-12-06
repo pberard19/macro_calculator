1d25e61fc40a51b66bc0f49a3d3119e3f4b63b67
Patrick Berard <patrickjamesberard@gmail.com>
2020-12-05 20:53:32 -0600

    Style updates & prototype food selectors

---
+++ components/calculator/food.js
@@ -0,0 +1,44 @@
+export const food = {
+  "proteins": {
+    "Chicken Breast": {
+      "cal": 47,
+      "fat": 1,
+      "carb": 0,
+      "pro": 9
+    },
+    "Steak": {
+      "cal": 77,
+      "fat": 5,
+      "carb": 0,
+      "pro": 7
+    },
+  },
+  "carbohydrates": {
+    "Rice": {
+      "cal": 37,
+      "fat": 0.1,
+      "carb": 8,
+      "pro": 0.8
+    },
+    "Pasta": {
+      "cal": 38,
+      "fat": 0.3,
+      "carb": 7,
+      "pro": 1.5
+    },
+  },
+  "fats": {
+    "Olive Oil": {
+      "cal": 250,
+      "fat": 29,
+      "carb": 0,
+      "pro": 0
+    },
+    "Avocado": {
+      "cal": 45,
+      "fat": 4,
+      "carb": 2,
+      "pro": 0.6
+    },
+  }
+}
