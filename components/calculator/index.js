a8b66dcce35d7ba1109a0df4ee5f9639e060c972
Patrick Berard <patrickjamesberard@gmail.com>
2020-12-06 21:35:59 -0600

    Do some basic math

--- components/calculator/index.js
+++ components/calculator/index.js
@@ -17,9 +17,55 @@ export const Calculator = () => {
   const [carbGoal, setCarbGoal] = useState(0);
   const [fatGoal, setFatGoal] = useState(0);
 
+  const [proteinOz, setProteinOz] = useState(0);
+  const [carbOz, setCarbOz] = useState(0);
+  const [fatOz, setFatOz] = useState(0);
+
   const [proteinChoice, setProteinChoice] = useState(proteinOptions[0]);
   const [carbChoice, setCarbChoice] = useState(carbOptions[0]);
   const [fatChoice, setFatChoice] = useState(fatOptions[0]);
+
+  const getFoodDescriptor = (choice, ounces) => {
+    return <div>
+    Stats (per oz):<br/>
+      Calories: {choice['cal']}cal x {ounces}oz = {Math.round(choice['cal'] * ounces)} cal<br/>
+      Fat: {choice['fat']}g x {ounces}oz = {Math.round(choice['fat'] * ounces)} g<br/>
+      Carbs: {choice['carb']}g x {ounces}oz = {Math.round(choice['carb'] * ounces)} g<br/>
+      Protein: {choice['pro']}g x {ounces}oz = {Math.round(choice['pro'] * ounces)} g<br/>
+    </div>
+  }
+
+  const getTotalDescriptor = () => {
+    const totalCals = (carbs[carbChoice]['cal'] * carbOz) +
+      (proteins[proteinChoice]['cal'] * proteinOz) +
+      (fats[fatChoice]['cal'] * fatOz);
+    const totalFat = (carbs[carbChoice]['fat'] * carbOz) +
+      (proteins[proteinChoice]['fat'] * proteinOz) +
+      (fats[fatChoice]['fat'] * fatOz);
+    const totalCarbs = (carbs[carbChoice]['carb'] * carbOz) +
+      (proteins[proteinChoice]['carb'] * proteinOz) +
+      (fats[fatChoice]['carb'] * fatOz);
+    const totalProteins = (carbs[carbChoice]['pro'] * carbOz) +
+      (proteins[proteinChoice]['pro'] * proteinOz) +
+      (fats[fatChoice]['pro'] * fatOz);
+
+    const getDeltaFromGoal = (total, goal) => {
+      const delta = goal - total;
+      let direction = '+';
+      if(delta === 0) return 'Hit your goal!';
+      if(delta > 0) return `+${Math.abs(delta)} grams above goal`;
+      if(delta < 0) return `-${Math.abs(delta)} grams below goal`;
+      return "WOMP";
+    }
+
+    return <div>
+      <h4>Totals</h4>
+      Calories: {totalCals}cal<br/>
+      Fat: {totalFat}g ({getDeltaFromGoal(totalFat, fatGoal)})<br/>
+      Carbs: {totalCarbs}g ({getDeltaFromGoal(totalCarbs, carbGoal)})<br/>
+      Protein: {totalProteins}g ({getDeltaFromGoal(totalProteins, proteinGoal)})<br/>
+    </div>
+  }
 
   return (
     <div>
@@ -37,21 +83,38 @@ export const Calculator = () => {
           <label>&nbsp;Fat (Grams/Day)</label>
         </div>
       </div>
+      <div className={styles.goalInputContainer} >
+        {getTotalDescriptor()}
+      </div>
 
       <div className={styles.selectorContainer}>
         <div className={styles.card}>
-          <h3 className={styles.selectorLabel}>Protein 🍖</h3>
-          <Dropdown options={proteinOptions} onChange={(e) => setProteinChoice(e.value)} value={proteinChoice} placeholder="Select an option" />
-        </div>
-        <div className={styles.card}>
           <h3 className={styles.selectorLabel}>Carbs 🍚</h3>
           <Dropdown options={carbOptions} onChange={(e) => setCarbChoice(e.value)} value={carbChoice} placeholder="Select an option" />
+          <div className={styles.goalInput} >
+            <input className={styles.numberInput} type="number" id="carbOz" name="carbOz" min="0" max="1000" value={carbOz} onChange={(event) => setCarbOz(event.target.value)} />
+            <label>&nbsp;Oz</label>
+          </div>
+          {getFoodDescriptor(carbs[carbChoice], carbOz)}
+        </div>
+        <div className={styles.card}>
+          <h3 className={styles.selectorLabel}>Protein 🍖</h3>
+          <Dropdown options={proteinOptions} onChange={(e) => setProteinChoice(e.value)} value={proteinChoice} placeholder="Select an option" />
+          <div className={styles.goalInput} >
+            <input className={styles.numberInput} type="number" id="proteinOz" name="proteinOz" min="0" max="1000" value={proteinOz} onChange={(event) => setProteinOz(event.target.value)} />
+            <label>&nbsp;Oz</label>
+          </div>
+          {getFoodDescriptor(proteins[proteinChoice], proteinOz)}
         </div>
         <div className={styles.card}>
           <h3 className={styles.selectorLabel}>Fats 🥑</h3>
           <Dropdown options={fatOptions} onChange={(e) => setFatChoice(e.value)} value={fatChoice} placeholder="Select an option" />
+          <div className={styles.goalInput} >
+            <input className={styles.numberInput} type="number" id="fatOz" name="fatOz" min="0" max="1000" value={fatOz} onChange={(event) => setFatOz(event.target.value)} />
+            <label>&nbsp;Oz</label>
+          </div>
+          {getFoodDescriptor(fats[fatChoice], fatOz)}
         </div>
-
       </div>
     </div>
   );
